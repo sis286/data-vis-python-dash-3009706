7bbe1320faf9f757b54a3d6dc3d7b0762a961aa3
Simon Schmitz <simon.schmitz@allvest.de>
2023-05-10 12:01:27 +0200

    Add gold prices to dash app.

--- 02_03/basic_dash_app.py
+++ 02_03/basic_dash_app.py
@@ -1,9 +1,23 @@
 import dash
-from dash import html
+import pandas as pd
+from dash import html, dcc
+import plotly.express as px
+
+
+data = pd.read_csv('precious_metals_prices_2018_2021.csv', usecols=['DateTime', 'Gold'])
+
+fig = px.line(data, x='DateTime', y='Gold', title='Gold Prices')
 
 app = dash.Dash(__name__)
-
-app.layout = html.P("Hello Dash!")
+app.title = 'Gold Prices'
+app.layout = html.Div(
+    id='app-container',
+    children=[
+        html.H1(app.title),
+        html.P('Results in USD/oz'),
+        dcc.Graph(figure=fig),
+    ]
+)
 
 if __name__ == "__main__":
     app.run_server(debug=True)
